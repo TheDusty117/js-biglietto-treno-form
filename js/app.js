679506efea3ef8e774c1628d3b06e10348ce2510
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-01 16:30:15 +0100

    FEAT query selector active on inputs and select(?)

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+console.log("working!")
+
+const inputTotalDistance = document.querySelector(
+  "input[name='distance']"
+  )
+
+const inputAgeElement = document.querySelector(
+  "select[name='age']"
+)
+
+const formBtnElement = document.querySelector(
+  "input[type='button']"
+)
+
+console.log(inputTotalDistance,inputAgeElement,formBtnElement)
+
+
+
+// let pricePerKm = 0.21;
+
+// let finalPrice = totalDistance * pricePerKm;
+
+// console.log(totalDistance)
